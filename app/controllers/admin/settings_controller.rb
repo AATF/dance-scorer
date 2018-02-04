2cf6e50555245c1ae7486ad2a2fe1824466babf9
Felix Wong <waf@fawong.com>
2018-02-03 17:54:34 -0800

    add update setting

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -2,4 +2,8 @@ class Admin::SettingsController < ApplicationController
   def edit
     @setting = Setting.find_by_id(params[:id])
   end
+
+  def update
+    Setting.update(var: params[:id], value: params[:setting][:value])
+  end
 end
