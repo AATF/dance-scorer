54809cdcb27d3154d4da417495891089f491b5ff
felix wong <felix.wong@waf.hk>
2025-10-12 19:46:59 -0700

    new settings

--- src/app/models/setting.rb
+++ src/app/models/setting.rb
@@ -1,3 +1,8 @@
 # RailsSettings Model
 class Setting < RailsSettings::Base
+  cache_prefix { "v1" }
+
+  # Define your fields
+  field :public_scores, type: :boolean, default: false
+  field :app_name, default: "AATF", type: :string
 end
