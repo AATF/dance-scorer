d3d37760e6945b343d33940c451f9e2016550115
felix wong <felix.wong@waf.hk>
2025-11-15 03:30:44 -0800

    sigh

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+#  devise_for :users, controllers: { sessions: 'users/sessions' }
 
   root "home#index"
 
   resources :admin, only: :index
-#  resources :sessions
+  resources :sessions
   resources :groups
   resources :users
   resources :dancers
